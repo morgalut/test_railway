6083f5188c96bc48fcf9b4cf2bf813dea7eb36ca
wagabage <wagabaga58@gmail.com>
2024-11-29 13:08:59 +0200

    add about page

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return "Peace of Mind App is Live!"
 
+@app.route('/health')
+def health():
+    return "OK", 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
